13e6ea77e291c714c26b1116990dda0f7fc33ceb
Yiding Wang <yiding.wang82@gmail.com>
2023-04-22 22:54:17 -0400

    initial commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    public class Circle
+    {
+    public double Radius { get; set; }
+
+    public Circle(double Radius)   
+    {
+            this.Radius = Radius;
+
+    }
+       public double Diameter()
+        {
+            double d = 2 * Radius;
+            return d;
+        }
+
+        public double Circumference()
+        {
+            double c = 2 * Radius * Math.PI;
+            return c;
+        }
+
+        public double Area()
+        {
+            double s = Radius * Radius * Math.PI;
+            return s;
+        }
+
+        public double Grow(double r)
+        {
+            double Radius = 2 * r;
+            return Radius;
+        }
+
+        public double GetRadius()
+        {
+            return Radius;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CircleObjects
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Circle Tester");
+            double r = RadiusEnter();
+            Console.WriteLine("Enter radius:" +r);
+            bool grow = true;
+            do
+            {
+                Circle objCircle = new Circle(r);
+                double diameter = objCircle.Diameter();
+                double circumference = objCircle.Circumference();
+                double area = objCircle.Area();
+                Console.WriteLine("Diameter: " + diameter);
+                Console.WriteLine("Circumference: " + circumference);
+                Console.WriteLine("Area: " + area);
+
+                bool continueGrow = ContinueGrow();
+                if (continueGrow)
+                {
+                    r = objCircle.Grow(r);
+                    continue;
+                }
+                else if (continueGrow==false)
+                {
+                    Console.WriteLine("Goodbye. The circle’s final radius is " + r);
+                    grow = false;
+                }
+            }while (grow);
+        }
+
+    public static double RadiusEnter()
+        {
+                try
+                {
+                    Console.WriteLine("Please input radius: ");
+                    double r = double.Parse(Console.ReadLine());
+                    return r;
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine(e.Message);
+                    Console.WriteLine("That was not a valid input, let's try that again");
+      
+                return RadiusEnter();
+                }
+        }
+
+        public static bool ContinueGrow()
+        {
+            bool grow = true;
+            do { 
+            Console.WriteLine("Should the circle grow? (y/n)");
+            string answer = Console.ReadLine().ToLower().Trim();
+            if (answer == "y")
+            { 
+               return true;
+
+            }
+            else if (answer == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input,let's try again!");
+                return ContinueGrow();
+            }
+            }while( grow );
+        }
+    }
+}
